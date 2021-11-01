69440ce871b3eaf536ce5f94f94933a8c8c714bc
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2021-11-01 14:10:39 -0300

    Deploy website - based on d916cc29303b4eed0d304e371d03b3f4eec5604d

--- dacfc009.49f28a86.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{101:function(a){a.exports=JSON.parse('{"allTagsPath":"/picpay-docs-digital-payments/change-log/tags","slug":"pagamentos","name":"pagamentos","count":1,"permalink":"/picpay-docs-digital-payments/change-log/tags/pagamentos"}')}}]);
