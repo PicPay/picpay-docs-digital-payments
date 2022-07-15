53d1d5b81a9304420958f5d1dfda4e370989954f
moacirjun <moacirjunior.eletro@gmail.com>
2022-07-15 19:27:37 -0300

    Deploy website - based on 71aeacf1185a1617ec2e12efec625eb7a88d0890

--- dc193de4.b6caa15e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{106:function(a){a.exports=JSON.parse('{"allTagsPath":"/picpay-docs-digital-payments/change-log/tags","slug":"payments","name":"payments","count":1,"permalink":"/picpay-docs-digital-payments/change-log/tags/payments"}')}}]);
