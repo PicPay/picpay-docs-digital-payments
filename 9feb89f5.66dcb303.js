1b1e919f523e7c6738535f05bb65cee17366c358
Vanessa Nascimento <vanessa.nascimento@picpay.com>
2022-09-27 18:01:10 -0300

    Deploy website - based on dc9c3c5ba9c12ff9ecdc803c25c884d15f43c3fd

---
+++ 9feb89f5.66dcb303.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{95:function(a){a.exports=JSON.parse('{"allTagsPath":"/picpay-docs-digital-payments/change-log/tags","slug":"integradores","name":"integradores","count":1,"permalink":"/picpay-docs-digital-payments/change-log/tags/integradores"}')}}]);
