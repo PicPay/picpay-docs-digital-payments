a96ed6c528b9ebdc32539db5d6b6690f9b984037
Alice Rodrigues <alice.santos@picpay.com>
2023-06-21 16:54:31 -0300

    Deploy website - based on 1f3b177868bd44cb4f66946a2fe4ae3834936be2

---
+++ dc193de4.2980f768.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[41],{113:function(a){a.exports=JSON.parse('{"allTagsPath":"/picpay-docs-digital-payments/change-log/tags","slug":"payments","name":"payments","count":1,"permalink":"/picpay-docs-digital-payments/change-log/tags/payments"}')}}]);
--- df8c6625.e0a60456.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{113:function(e){e.exports=JSON.parse('{"permalink":"/picpay-docs-digital-payments/change-log","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null,"blogDescription":"Mudan\xe7as e novidades a respeito de nossas solu\xe7\xf5es de pagamento digital","blogTitle":"Change Log"}')}}]);
