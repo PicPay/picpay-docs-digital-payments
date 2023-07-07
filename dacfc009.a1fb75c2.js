af7e236b413e7aa52cd555515e83df3f81614f61
André <andre.parpaiola@picpay.com>
2023-07-07 14:38:38 -0300

    Deploy website - based on 305eed99e6a7aff2bfc0d4af8192341b3aa1c2e3

---
+++ dacfc009.a1fb75c2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{111:function(a){a.exports=JSON.parse('{"allTagsPath":"/picpay-docs-digital-payments/change-log/tags","slug":"pagamentos","name":"pagamentos","count":1,"permalink":"/picpay-docs-digital-payments/change-log/tags/pagamentos"}')}}]);
