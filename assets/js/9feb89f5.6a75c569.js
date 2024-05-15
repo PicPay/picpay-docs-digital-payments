dcddd71c85b62c2954b52b890621d3275291034c
Diego Rustnnes Bandeira <638445+rustnnes@users.noreply.github.com>
2024-05-15 16:38:56 -0300

    Deploy website - based on 13d51508e2582c5cd802d7ebecd885470a0fa25a

---
+++ assets/js/9feb89f5.6a75c569.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpicpay=self.webpackChunkpicpay||[]).push([[9221],{54424:a=>{a.exports=JSON.parse('{"allTagsPath":"/picpay-docs-digital-payments/change-log/tags","slug":"integradores","name":"integradores","count":1,"permalink":"/picpay-docs-digital-payments/change-log/tags/integradores"}')}}]);
